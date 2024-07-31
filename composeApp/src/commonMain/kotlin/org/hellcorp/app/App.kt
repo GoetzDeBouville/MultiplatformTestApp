fa7f83b1fe3d98ddc277c8746b350934fe377b05
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-31 13:08:27 +0300

    add screen files, enum nav class

--- composeApp/src/commonMain/kotlin/org/hellcorp/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/hellcorp/app/App.kt
@@ -1,11 +1,20 @@
 package org.hellcorp.app
 
 import androidx.compose.runtime.*
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.hellcorp.app.theme.AppTheme
 
 @Composable
 internal fun App() = AppTheme {
-    
+    MultiPlatformTestApp()
+}
+
+@Composable
+internal fun MultiPlatformTestApp(
+    navCotroller : NavHostController = rememberNavController()
+){
+
 }
 
 internal expect fun openUrl(url: String?)
---
+++ composeApp/src/commonMain/kotlin/org/hellcorp/app/features/feed/FeedScreen.kt
@@ -0,0 +1,2 @@
+package org.hellcorp.app.features.feed
+
